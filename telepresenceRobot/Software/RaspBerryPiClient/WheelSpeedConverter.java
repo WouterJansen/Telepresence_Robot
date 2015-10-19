183750345dc91cd5d2998e26eecc410942ae2042
WouterJMW <wouterjansen4@gmail.com>
2015-10-19 14:46:50 +0200

    wheelspeedconverter toegevoegd

---
+++ telepresenceRobot/Software/RaspBerryPiClient/WheelSpeedConverter.java
@@ -0,0 +1,37 @@
+package RaspBerryPiClient;
+
+//class to convert a set of wheel speeds in a string containing wheel direction and speed in value between 0 and 255
+// to make it easier for the Atmel Microprocessor to read it in. This string can then be send over Serial UART. 
+public class WheelSpeedConverter {
+	
+	public WheelSpeedConverter(){
+	}
+	
+	//conversion method from wheel speed.
+	public static String Conversion(String wheelSpeeds){
+		//split string into two floats again
+		String[] parts = wheelSpeeds.split(",");
+		float lwheel = Float.parseFloat(parts[0]);
+		float rwheel = Float.parseFloat(parts[1]);
+		
+		//Determine direction
+		int wheelDirection = 0;
+		if(lwheel >= 0)wheelDirection = 1;
+		else if (lwheel < 0)wheelDirection = -1;
+		
+		//Conversion of number between 0 and 1 to between 0 and 255
+		int lwheelC = Math.round((Math.abs(lwheel)) * 255);
+		int rwheelC = Math.round((Math.abs(rwheel)) * 255);
+		
+		//combining into one string
+		String combined = wheelDirection + " " + lwheelC + " " + rwheelC;				
+		return combined;
+	}
+	
+	   public static void main(String argv[]){
+		//test string
+		String receivedString = "0.81,0.45";
+	    String display = Conversion(receivedString);
+		System.out.println(display);
+	}
+}
